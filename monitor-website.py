002a711dd0f523206412ced619f6eae4ef1507c9
Tilemaxos Alexiadis <talexiadis@lancom.gr>
2024-04-16 14:54:49 +0100

    add send email on error and handle email erro

--- monitor-website.py
+++ monitor-website.py
@@ -1,8 +1,28 @@
 import requests
+import smtplib
+import os
 
-response = requests.get('xxxx')
-print(response.status_code)
-if response.status_code == 200:
-  print('Application is running successfully')
-else:
-  print ('Application Down. Fix it!')
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+def send_notification(email_msg):
+  with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    msg = f"Subject: SITE DOWN\n {email_msg} "
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+
+
+try:
+  response = requests.get('xxxx')
+  if response.status_code == 200:
+    print('Application is running successfully')
+  else:
+    print ('Application Down. Fix it!')
+    msg = f'Application returned {response.status_code}'
+    send_notification(msg)
+except Exception as ex:
+  with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+      msg = f"Subject: SITE DOWN\n Application not accessible at all"
+      send_notification(msg)
